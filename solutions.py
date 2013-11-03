15e29fe2bbdff92c22d0063acca5672978c93010
elewis <eclewis21@gmail.com>
2013-11-02 20:17:47 -0700

    problem 18 solution

--- solutions.py
+++ solutions.py
@@ -366,6 +366,40 @@ def problem17():
         total += len(word)
     return total + 11 # to include 1000
 
+def problem18():
+    """
+    By starting at the top of the triangle below and moving to adjacent
+    numbers on the row below, the maximum total from top to bottom is 23.
+
+    Find the maximum total from top to bottom of the triangle below:
+    """
+    triangle = [
+        [75],
+        [95, 64],
+        [17, 47, 82],
+        [18, 35, 87, 10],
+        [20,  4, 82, 47, 65],
+        [19,  1, 23, 75,  3, 34],
+        [88,  2, 77, 73,  7, 63, 67],
+        [99, 65, 04, 28,  6, 16, 70, 92],
+        [41, 41, 26, 56, 83, 40, 80, 70, 33],
+        [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+        [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+        [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+        [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+        [63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+        [04, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]
+    ]
+    # Destructive dynamic programming, bubbles up max path length
+    depth = len(triangle)-2
+    while depth >= 0:
+        row = triangle[depth]
+        children = triangle[depth+1]
+        for i in xrange(len(row)):
+            row[i] += max(children[i], children[i+1])
+        depth -= 1
+    return triangle[0][0]
+
 def problem20():
     """
     Find the sum of the digits in the number 100!
