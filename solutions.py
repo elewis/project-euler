12d0c3d44ad85637e6d695bd5580199582661312
elewis <eclewis21@gmail.com>
2013-11-02 22:53:23 -0700

    problem 21 solution

--- solutions.py
+++ solutions.py
@@ -457,6 +457,33 @@ def problem20():
         number /= 10
     return total
 
+def problem21():
+    """
+    Let d(n) be defined as the sum of proper divisors of n (numbers less than
+    n which divide evenly into n). If d(a) = b and d(b) = a, where a != b, then
+    a and b are an amicable pair and each of a and b are called amicable
+    numbers.
+
+    For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22,
+    44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1,
+    2, 4, 71 and 142; so d(284) = 220.
+
+    Evaluate the sum of all the amicable numbers under 10000.
+    """
+    limit = 10000
+    sieve = [[] for i in xrange(limit)]
+    total = 0
+
+    for i in xrange(1, limit):
+        if sieve[i]:
+            pair = sum(sieve[i]), i
+            if pair[0] < pair[1] and sum(sieve[pair[0]]) == pair[1]:
+                total += pair[0] + pair[1]
+        for j in xrange(2 * i, limit, i):
+            sieve[j].append(i)
+    return total
+
+
 def problem30():
     """
     Find the sum of all the numbers that can be written as the sum of fifth
