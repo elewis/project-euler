3a3cf47b2fd2d5380c1ccfbdf7989c57915304ce
elewis <eclewis21@gmail.com>
2013-12-03 23:22:00 -0800

    Problem 38 solution

--- solutions.py
+++ solutions.py
@@ -769,6 +769,41 @@ def problem37():
             total += i
     return total
 
+def problem38():
+    """
+    Take the number 192 and multiply it by each of 1, 2, and 3:
+
+        192 x 1 = 192
+        192 x 2 = 384
+        192 x 3 = 576
+
+    By concatenating each product we get the 1 to 9 pandigital, 192384576.
+    We will call 192384576 the concatenated product of 192 and (1,2,3)
+
+    The same can be achieved by starting with 9 and multiplying by 1, 2, 3,
+    4, and 5, giving the pandigital, 918273645, which is the concatenated
+    product of 9 and (1,2,3,4,5).
+
+    What is the largest 1 to 9 pandigital 9-digit number that can be formed as
+    the concatenated product of an integer with (1,2, ... , n) where n > 1?
+    """
+    limit = 10000
+    best = 0
+
+    def is_pandigital(num_str):
+        return ''.join(sorted(num_str)) == "123456789"[:len(num_str)]
+
+    for i in xrange(limit):
+        num_str = ""
+        for j in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+            num_str += str(i * j)
+            if is_pandigital(num_str) and int(num_str) > best:
+                best = int(num_str)
+                break
+            elif len(num_str) > 9:
+                break
+    return best
+
 def problem39():
     """
     If p is the perimeter of a right angle triangle with integral length sides,
