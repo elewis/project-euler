c15f48d7e958fc161d6d5b2710e6b73820f971f3
Ethan Lewis <eclewis21@gmail.com>
2014-01-31 12:30:43 -0800

    Problem 43 solution

--- solutions.py
+++ solutions.py
@@ -1039,6 +1039,41 @@ def problem42():
             total += score_count[t]
     return total
 
+def problem43():
+    """
+    The number, 1406357289, is a 0 to 9 pandigital number because it is made
+    up of each of the digits 0 to 9 in some order, but it also has a rather
+    interesting sub-string divisibility property.
+
+    d2d3d4=406 is divisible by 2
+    d3d4d5=063 is divisible by 3
+    d4d5d6=635 is divisible by 5
+    d5d6d7=357 is divisible by 7
+    d6d7d8=572 is divisible by 11
+    d7d8d9=728 is divisible by 13
+    d8d9d10=289 is divisible by 17
+
+    Find the sum of all 0 to 9 pandigital numbers with this property.
+    """
+
+    def permutations(digits):
+        if len(digits) > 0:
+            for i in xrange(len(digits)):
+                for p in permutations(digits[:i] + digits[i+1:]):
+                    yield digits[i] + p
+        else:
+            yield ''
+
+    factors = 2, 3, 5, 7, 11, 13, 17
+    total = 0
+    for digits in permutations('0123456789'):
+        for i in xrange(1, 8):
+            if int(digits[i:i+3]) % factors[i-1] != 0:
+                break
+        else:
+            total += int(digits)
+    return total
+
 def problem50():
     """
     Which prime, below one-million, can be written as the sum of the most
