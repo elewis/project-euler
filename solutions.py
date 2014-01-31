aa8b78bb45aecefc24dcf9b7a26c197f238a3088
Ethan Lewis <eclewis21@gmail.com>
2014-01-31 11:56:47 -0800

    Problem 42 solution

--- solutions.py
+++ solutions.py
@@ -990,6 +990,55 @@ def problem41():
                     best = i
     return best
 
+def problem42():
+    """
+    The nth term of the sequence of triangle numbers is given by,
+    tn = (1/2)n(n+1); so the first ten triangle numbers are:
+
+    1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+
+    By converting each letter in a word to a number corresponding to its
+    alphabetical position and adding these values we form a word value.
+    For example, the word value for SKY is 19 + 11 + 25 = 55 = t10. If the
+    word value is a triangle number then we shall call the word a triangle
+    word.
+
+    How many of the words in words.txt are triangle words?
+    """
+    import requests
+
+    words = requests.get('http://projecteuler.net/project/words.txt').text
+    words = words.strip().split(',')
+    score_count = {}
+
+    # Generates all triangles up to and including t_n
+    def triangles(n):
+        for i in xrange(1, n+1):
+            yield i * (i+1) / 2
+
+    def score(word):
+        total = 0
+        for c in word:
+            total += ord(c) - 96 # Ascii offset
+        return total
+
+    max_score = 0
+    for word in words:
+        word = word.strip('"').lower()
+        word_score = score(word)
+        if word_score > max_score:
+            max_score = word_score
+        if word_score in score_count:
+            score_count[word_score] += 1
+        else:
+            score_count[word_score] = 1
+
+    total = 0
+    for t in triangles(max_score):
+        if t in score_count:
+            total += score_count[t]
+    return total
+
 def problem50():
     """
     Which prime, below one-million, can be written as the sum of the most
