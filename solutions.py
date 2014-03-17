5078671320da3c0fe014668aee7e515ec2c225a3
Ethan Lewis <eclewis21@gmail.com>
2014-03-16 21:41:40 -0700

    Problem 53 solution

--- solutions.py
+++ solutions.py
@@ -1229,6 +1229,39 @@ def problem52():
             return i
         i += 1
 
+def problem53():
+    """
+    There are exactly ten ways of selecting three from five, 12345:
+
+    123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+    In combinatorics, we use the notation, 5C3 = 10.
+
+    In general,
+    nCr =   
+    n!
+    r!(n-r)!
+    ,where r <= n, n! = nx(n-1)x...x3x2x1, and 0! = 1.
+
+    It is not until n = 23, that a value exceeds one-million: 23C10 = 1144066.
+
+    How many, not necessarily distinct, values of  nCr, for 1 <= n <= 100, are greater than one-million?
+    """
+    limit = 101
+    fac = [1]
+
+    def c(n, r):
+        return fac[n] / (fac[r] * fac[n-r])
+
+    for i in xrange(1, limit):
+        fac.append(fac[-1] * i)
+    count = 0
+    for n in xrange(1, limit):
+        for r in xrange(1, n+1):
+            if c(n, r) > 1000000:
+                count += 1
+    return count
+
 def problem59():
     """
     A modern encryption method is to take a text file, convert the bytes to
